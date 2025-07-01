dd8ffcc18e4444dec3b1aa4724ae06f849d5442b
jetronet30 <jabakhuluzauri@gmail.com>
2025-07-01 12:43:41 +0000

    Pa

--- src/main/java/com/jaba/p2_t/controllers/fragmentscontroller/ExtCon.java
+++ src/main/java/com/jaba/p2_t/controllers/fragmentscontroller/ExtCon.java
@@ -3,8 +3,6 @@ package com.jaba.p2_t.controllers.fragmentscontroller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.jaba.p2_t.extensions.ExtenService;
-import com.jaba.p2_t.extensions.Extensions;
 
 import lombok.RequiredArgsConstructor;
 
