4780bf7dd75bdf4080de64f2b2a35b0ce43c53a8
jetronet30 <jabakhuluzauri@gmail.com>
2025-08-27 10:38:33 +0000

    ROOMS_STD

--- src/main/java/com/jaba/p2_t/controllers/pmscontrollers/RoomController.java
+++ src/main/java/com/jaba/p2_t/controllers/pmscontrollers/RoomController.java
@@ -1,13 +1,12 @@
 package com.jaba.p2_t.controllers.pmscontrollers;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/pms")
+
+@Controller
 public class RoomController {
 
     @PostMapping("/rooms")
@@ -18,7 +17,7 @@ public class RoomController {
     @PostMapping("/roomstatus")
     public String testPost(@RequestParam String room, @RequestParam String status) {
         System.out.println(room+"  test  "+status);
-        return "test";
+        return "fragments/rooms";
     }
 
 
